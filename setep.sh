1c5a3fcb6703e1251de0a4775af176d5631c1325
anonymous <64162091+anonymous770@users.noreply.github.com>
2021-03-08 12:49:30 +0530

    Update setep.sh

--- setep.sh
+++ setep.sh
@@ -2,3 +2,4 @@ chmod +x *
 cp -r kex /data/data/com.termux/files/usr/bin
 cp -r stop /data/data/com.termux/files/usr/bin
 cp -r restart /data/data/com.termux/files/usr/bin
+nano ../usr/etc/bash.bashrc
